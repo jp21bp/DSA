0408336a255f807fad9aae360efe42e9317c596e
jp <jowapra@gmail.com>
2026-01-25 09:00:21 -0500

    added bubble sort algo

--- structures.py
+++ structures.py
@@ -617,3 +617,61 @@ class HashTable:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######## Sorting algorithms
+##### Bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        swapped = False
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped: break
+    return arr
+
+
+
+
+
+
+
+
