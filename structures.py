cd0c1ce128e48c81bfb88e170b395d8207cb33aa
jp <jowapra@gmail.com>
2026-01-27 15:12:16 -0500

    Added selection sort

--- structures.py
+++ structures.py
@@ -704,3 +704,109 @@ def insertion_sort(arr):
     return arr
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##### Selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]: min_index = j
+        min_value = arr[min_index]
+        arr[i] = min_value
+    return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
