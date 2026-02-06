50fb7c25c5e63b61a142dbd79cd27e9d9ecb6ba0
jp <jowapra@gmail.com>
2026-02-06 15:23:58 -0500

    added linear search and binary search

--- structures.py
+++ structures.py
@@ -917,3 +917,148 @@ def counting_sort(arr, order: []):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### Search algorithms
+#### Linear search
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target: return i
+    return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Binary search
+### Recursive
+def binary_search_rec(arr, target, left, right):
+    if left < right: return -1
+    mid = left + (right - left)//2
+    if arr[mid] == target: return mid
+    elif arr[mid] < target:
+        return binary_search_rec(arr, target, mid + 1, right)
+    else: return binary_search_rec(arr, target, left, mid - 1)
+
+### Iterative
+def binary_search_iter(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = left + (right - left)//2
+        if arr[mid] == target: return mid
+        elif arr[mid] < target: left = mid + 1
+        else: right = mid - 1
+    return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
